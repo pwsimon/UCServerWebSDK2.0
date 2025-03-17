f00c17be7b3faf76f2308b5d95762c26aab35a12
Peter Simon <psi@estos.de>
2025-03-17 15:27:11 +0100

    work with NTLM configured server

--- browser-client/ntlm-connect/ntlm-connect.js
+++ browser-client/ntlm-connect/ntlm-connect.js
@@ -1,4 +1,25 @@
-window.onload(() => {
-	console.log("ntlm-connect.js loaded():", document.location);
-	// fetch('http://localhost:3000/ntlm', {});
-});
+window.onload = () => {
+	console.log("ntlm-connect.js loaded():", document.location.origin);
+	fetch(document.location.origin + "/ws/client/ucserverversion")
+	.then(response => response.json())
+	.then(oUCServerVersion => {
+		console.log("UCServerVersion:", oUCServerVersion);
+		const oNegotiate = {
+			negotiate: {
+				iClientProtocolVersion: oUCServerVersion.ucserverprotocolversion // 61, 70
+			}
+		};
+		return fetch(document.location.origin + "/ws/client/createsession?clientappid=9", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"X-EPID": "generierte GUID",
+			},
+			body: JSON.stringify(oNegotiate)
+		});
+	})
+	.then(response => response.json())
+	.then(oSession => {
+		console.log("Session:", oSession.sessionid);
+	});
+};
